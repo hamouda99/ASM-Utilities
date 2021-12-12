eb21daa54193df1184597433c00cffeb26e2a732
hamouda99 <amir_hamouda@yahoo.com>
2021-12-12 18:58:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ASM-Utilities
+This is a Telegram Bot that will be used from ASM-PoliMi to speed up some booring task
---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import telebot
+
+API_KEY =  "5077024786:AAEpTYGCsHm--t1FXU2fJJqfPKHvtVRvhII"
+bot = telebot.TeleBot(API_KEY)
+print("wprking")
+@bot.message_handler(commands=['greet'])
+def greet (message):
+    bot.reply_to(message, "Bella li")
+
+bot.polling()
